6fa43e6b6c95aece76d969c5f2135e3a0804486e
alight <limingwxm@126.com>
2017-04-01 19:55:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var srcFilePath = arguments[0] || null;
 if (!srcFilePath) {
     logInfo("第一个参数是转码相对路径，请确保填写了")
 }
-if (srcFilePath.indexOf("www/js") == -1) {
-    srcFilePath = path.join(rootPath, "www/js", srcFilePath);
-} else {
-    srcFilePath = path.join(rootPath, srcFilePath);
-}
+// if (srcFilePath.indexOf("www/js") == -1) {
+//     srcFilePath = path.join(rootPath, "www/js", srcFilePath);
+// } else {
+srcFilePath = path.join(rootPath, srcFilePath);
+// }
 
 var dstFilePath = getDstFilePath(srcFilePath);
-console.log("转换源文件",srcFilePath);
-console.log("转目的文件",dstFilePath);
+console.log("转换源文件", srcFilePath);
+console.log("转目的文件", dstFilePath);
 
 transfer(srcFilePath, dstFilePath, true)
 
